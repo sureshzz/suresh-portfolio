e617658f82e20113313555b17a3a6b17a319c0f7
sureshzz <sureshthapamagar8914@gmail.com>
2024-06-05 23:16:24 +0545

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar'
 
 export default function App() {
   return (
-    <div className='bg-blue-950 min-h-screen'>
+    <div className=''>
         <Navbar />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 
 const Navbar = () => {
-  const elementCSS = "mx-6 text-white"
+  const elementCSS =
+    "text-white p-4 m-4 hover:scale-100 hover:scale-110 duration-50 cursor-pointer hover:underline";
   return (
     <div>
-      <div>
-        <ul className='flex flex-row justify-end p-4 bg-transparent'>
+      <div className=" bg-slate-900  h-20 w-full flex flex-row justify-between justify-items-center">
+        <div className="font-logo text-white w-40">
+          <h1 className='text-5xl mx-1 p-4'>Suresh</h1>
+        </div>
+        <ul className=" flex flex-row ">
           <li className={`${elementCSS}`}>About</li>
           <li className={`${elementCSS}`}>Work</li>
           <li className={`${elementCSS}`}>Contact</li>
         </ul>
       </div>
     </div>
-  )
+  );
 } 
 
 export default Navbar
