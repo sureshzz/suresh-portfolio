9194e5a9ea5e216317dd1c4aaf29628dae7c91c3
sureshzz <sureshthapamagar8914@gmail.com>
2024-06-06 16:09:04 +0545

    nav.resp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../App.css";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-  const elementCSS =
-    "text-white p-4 m-4 hover:scale-100 hover:scale-110 duration-50 cursor-pointer hover:underline";
-  return (
-    <div>
-      <div className=" bg-slate-900  h-20 w-full flex flex-row justify-between justify-items-center">
-        <div className="font-logo text-white w-40">
-          <h1 className='text-5xl mx-1 p-4'>Suresh</h1>
-        </div>
-        <ul className=" flex flex-row ">
-          <li className={`${elementCSS}`}>About</li>
-          <li className={`${elementCSS}`}>Work</li>
-          <li className={`${elementCSS}`}>Contact</li>
-        </ul>
-      </div>
-    </div>
-  );
-} 
+  const [nav, setnav] = useState(false);
 
-export default Navbar
+  const elementCSS =
+    "hover:scale-100 hover:scale-110 duration-50 cursor-pointer hover:text-white px-4";
+  const links = [
+    { id: 1, link: "About" },
+    { id: 2, link: "Works" },
+    { id: 3, link: "Skills" },
+    { id: 4, link: "Contact" },
+  ];
+
+  return (
+      <div className=" bg-slate-900  h-20 w-full flex flex-row justify-between items-center px-4">
+        <div className="font-logo text-white">
+          <h1 className="text-5xl mx-1 p-4 hover-rotate-360 duration-100">
+            Suresh
+          </h1>
+        </div>
+        <ul className="hidden md:flex flex-row text-gray-400 gap-2">
+          {links.map(({ id, link }) => (
+            <li className={`${elementCSS}`} key={id}>
+              {link}
+            </li>
+          ))}
+        </ul>
+        <div
+          className="md:hidden cursor-pointer pr-0.4 z-10 text-white"
+          onClick={() => {
+            setnav(!nav);
+          }}
+        >
+          {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+        </div>
+        {nav && (
+          <ul className=" h-screen w-full flex flex-col justify-center items-start text-white bg-red-300 absolute top-0 right-0">
+            {links.map(({ id, link }) => (
+              <li
+                key={id}
+                className="cursor-pointer text-2xl text-white"
+              >
+                {link}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+  );
+};
+
+export default Navbar;
